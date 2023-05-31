7d62c75ba8e281856f37783050eb0dc1da7eafed
Muhammad Hussein Fattahizadeh <m@mhf.ir>
2023-05-31 11:30:13 +0330

    fix: namespace generator

--- template/node/namespace.py
+++ template/node/namespace.py
@@ -5,36 +5,50 @@ import re
 import random
 from urllib.parse import urlparse
 
+
 def main():
     parser = argparse.ArgumentParser(description='Namespace Generator')
     parser.add_argument('--url', type=str, help='URL argument')
+    parser.add_argument('--domain', type=str, help='Domain argument')
 
     args = parser.parse_args()
+    if args.url == None and args.domain == None:
+        raise Exception("domain or url required")
 
-    u = urlparse(args.url)
+    url_string = ""
+    if args.url != None:
+        url_string = args.url
+    elif args.domain != None:
+        url_string = "https://" + args.domain
+
+    u = urlparse(url_string)
 
     if u.hostname == None:
-        raise Exception("invalid url")
+        raise Exception("invalid url or domain")
 
-    d = str(u.hostname).replace('www.', '')
+    hostname = u.hostname.encode("idna").decode('utf-8')
+
+    d = str(hostname).replace('www.', '')
     d = re.sub(r'[.]+', '-', d)
     d = re.sub(r'[-]+', '-', d)
     d = d.strip('-')
 
-    if len(d) > 22:
-        d = d[-22:]
+    if len(d) > 24:
+        d = d[-24:]
 
     d = d.strip('-')
 
     rnd = ''.join(
         random.choice(string.ascii_lowercase + string.digits)
-        for _ in range(27 - len(d))
+        for _ in range(3)
     )
 
-    stages = ['d', 't', 's', 'p']
+    stages = ['dev', 'test', 'stage']
+    print(d + '-' + rnd)
     for s in stages:
         r = d + '-' + s + '-' + rnd
         print(r)
+
 
 if __name__ == '__main__':
     main()
